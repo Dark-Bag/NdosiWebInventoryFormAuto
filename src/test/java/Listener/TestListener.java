bc971a072d0e7cc019d075e87ea6ba32009b703c
Samkelo Mkwenkwe <mkwenkwesamkelo@gmail.com>
2026-03-02 21:55:31 +0200

    adding a html report

---
+++ src/test/java/Listener/TestListener.java
@@ -0,0 +1,33 @@
+package Listener;
+
+import com.aventstack.extentreports.*;
+import org.testng.*;
+import Utils.ExtentManager;
+
+public class TestListener implements ITestListener {
+
+    private static final ExtentReports extent = ExtentManager.getInstance();
+    private static final ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
+
+}

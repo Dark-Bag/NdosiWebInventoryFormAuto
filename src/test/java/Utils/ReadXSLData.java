e50b4dbfaf7d4ca057c5e769736a1a38fd31136b
Samkelo Mkwenkwe <mkwenkwesamkelo@gmail.com>
2026-03-19 21:33:46 +0200

    assessment

--- src/test/java/Utils/ReadXSLData.java
+++ src/test/java/Utils/ReadXSLData.java
@@ -1,4 +1,23 @@
 package Utils;
 
+
+import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+
+
 public class ReadXSLData {
+    @DataProvider(name = "testData")
+    public Object[][] getData(Method method) throws FileNotFoundException {
+        String excelSheetName = method.getName();
+        File file = new File(System.getProperty("user.dir") + "/src/test/resources/testdata/TestData.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+
+    }
+
+
 }
